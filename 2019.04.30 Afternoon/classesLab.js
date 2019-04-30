cbe587ea9d026af5b23be8e92b3d17c95c86c6a9
Mike Feldberg <mikefeldberg@gmail.com>
2019-04-30 14:57:46 -0700

    finish class lab. bank accounts

---
+++ 2019.04.30 Afternoon/classesLab.js
@@ -0,0 +1,47 @@
+class BankAccount {
+    constructor(ownerName, balance, acctNum) {
+        this.ownerName = ownerName;
+        this.balance = balance;
+        this.acctNum = acctNum;
+    }
+
+    deposit(amtDeposited) {
+        this.balance += amtDeposited;
+        console.log(`Deposit complete. Your new balance is ${this.balance}`);
+    }
+
+    withdraw(amtWithdrawn) {
+        this.balance += amtWithdrawn;
+        console.log(`Withdrawal complete. Your new balance is ${this.balance}`);
+    }
+}
+
+class CheckingAccount extends BankAccount {
+    constructor(ownerName, balance, acctNum, overdraftEnabled) {
+        super(ownerName, balance, acctNum);
+        this.overdraftEnabled = overdraftEnabled;
+    }
+    withdraw(amtWithdrawn) {
+        if (this.overdraftEnabled == true && amtWithdrawn > this.balance) {
+            this.balance = (this.balance - amtWithdrawn) - 30
+            console.log(`Withdrawal complete. Your account is now overdrawn. Your new balance is ${this.balance}, which includes an overdraft charge of \$30.`);
+        } else if (this.overdraftEnabled == false && amtWithdrawn > this.balance) {
+            console.log(`Sorry, you don't have enough funds to withdraw that amount. Your current balance is ${this.balance}`);
+        } else {
+            this.balance += amtWithdrawn;
+            console.log(`Withdrawal complete. Your new balance is ${this.balance}`);
+        }
+    }
+}
+
+class SavingsAccount extends BankAccount {
+    constructor(ownerName, balance, acctNum) {
+        super(ownerName, balance, acctNum);
+    }
+    withdraw() {
+        console.log('This is a Savings Accounts. You cannot make a withdrawal at this time.');
+    }
+}
+
+var myAcct = new CheckingAccount('Mike', 12345, 50, true);
+console.log(myAcct);
