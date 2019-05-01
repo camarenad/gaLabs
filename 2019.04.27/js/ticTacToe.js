b8f59b07729cdd5cbdd24e8839c5c5d2327b06c1
Mike Feldberg <mikefeldberg@gmail.com>
2019-05-01 16:58:27 -0700

    finish working tic tac toe

--- 2019.04.27/js/ticTacToe.js
+++ 2019.04.27/js/ticTacToe.js
@@ -1,84 +1,90 @@
-(function() {
+var player
 
-    // function initializeGame(){
-    var board = new Array(9).fill(null);
-    
-    var players = {
-		'1': {
-			name: 'One',
-			score: 0
-		},
-		'-1': {
-			name: 'Two',
-			score: 0
-		}
-	};
-    console.log('game init');
-    // }
-    
-    var board = new Array(9).fill(null);
+var winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];    
 
-    var neededToWin = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [2, 5, 7],
-        [5, 7, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
+var xBoard = [null, null, null, null, null, null, null, null, null];
+var oBoard = [null, null, null, null, null, null, null, null, null];
+var board = [null, null, null, null, null, null, null, null, null];
 
-    $('#resetGame').click(function() {
-        initializeGame();
-    });
+var boxes = document.querySelectorAll('td div');
 
-    var handleClick = $('#board').click(function(event){
-        if (player === 0) {
-            event.target.innerHTML = 'X';
-            document.getElementById('turnIndicator').innerHTML = 'Player Two Turn'
-            player ++;
-            event.target.style.pointerEvents.none;
-            board[event.target] = '1'
-            console.log(board)
-            console.log('et ' + event.target)
+document.querySelector('#resetGame').addEventListener('click', initGame);
+
+
+function handleClick(event) {
+    var idx = parseInt(event.target.id.replace('box', ''));
+    board[idx] = 1;
+    if (player === 1) {
+        xBoard[idx] = 1;
+    } else {
+        oBoard[idx] = 1;
+    }
+
+    render();
+    getWinner();
+}
+
+function initGame() {
+    clearBoard();
+    player = 1;
+    $('#turnIndicator').html('Player One Turn');
+    document.querySelector('table').addEventListener('click', handleClick);
+}
+
+function render() {;
+    // console.log('entering render')
+    // console.log(event.target.innerHTML)
+    if (event.target.innerHTML === '<div></div>') {
+        if (player == 1) {
+            event.target.innerHTML = '<div>X</div>';
+            $('#turnIndicator').html('Player Two Turn');
         } else {
-            event.target.innerHTML = 'O';
-            document.getElementById('turnIndicator').innerHTML = 'Player One Turn'
-            board[event.target] = '1'
-            player --;
-            console.log(board)
+            event.target.innerHTML = '<div>O</div>';
+            var idx = parseInt(event.target.id.replace('box', ''));
+            $('#turnIndicator').html('Player One Turn');
         }
-    });
-    
-    var addBoardListen = document.getElementById('board', handleClick);
+        player *= -1;
+    }
+}
 
-    addBoardListen.addEventListener('click', function(event){
+function add(accumulator, a) {
+    return accumulator + a;
+}
 
-    });
+function clearBoard() {
+    $('table div').html('');
+    xBoard = [null, null, null, null, null, null, null, null, null];
+    oBoard = [null, null, null, null, null, null, null, null, null];
+    board = [null, null, null, null, null, null, null, null, null];
+}
 
+function getWinner() {
+    for (var i = 0; i < winningCombinations.length; i++) {
+        if (Math.abs(xBoard[winningCombinations[i][0]] + xBoard[winningCombinations[i][1]] + xBoard[winningCombinations[i][2]]) === 3) {
+            $('#turnIndicator').html('Congrats Player One!');
+            document.querySelector('table').removeEventListener('click', handleClick);
+        } 
+    }
+    for (var i = 0; i < winningCombinations.length; i++) {
+        if (Math.abs(oBoard[winningCombinations[i][0]] + oBoard[winningCombinations[i][1]] + oBoard[winningCombinations[i][2]]) === 3) {
+            $('#turnIndicator').html('Congrats Player Two!');
+            document.querySelector('table').removeEventListener('click', handleClick);
+        }
+    }
+    for (var i = 0; i < winningCombinations.length; i++) {
+        if (board.reduce(add) == 9) {
+            $('#turnIndicator').html('Rats, another tie!');
+        }
+    }
+}
 
-
-    // $('#board').click(function(evt){
-    //     handleClick();
-    //     console.log('click');
-    // });
-
-
-    // function handleClick(event) {
-    //     if (player === 0) {
-    //         evt.target.innerHTML = 'X';
-    //         document.getElementById('turnIndicator').innerHTML = 'Player Two Turn'
-    //         // board[event.target] = '1'
-    //         player ++;
-    //     } else {
-    //         evt.target.innerHTML = 'O';
-    //         document.getElementById('turnIndicator').innerHTML = 'Player One Turn'
-    //         // board[event.target] = '1'
-    //         player --;
-    //     }
-    // }
-
-    // initializeGame();
-
-})();
+initGame();
