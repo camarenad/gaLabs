f38b65904979a78fc6756503d0c1a77905893b1c
Mike Feldberg <mikefeldberg@gmail.com>
2019-05-03 13:19:55 -0700

    finish back end game logic

--- 2019.05.02 P1 - Mastermind/js/mastermind.js
+++ 2019.05.02 P1 - Mastermind/js/mastermind.js
@@ -1,72 +1,89 @@
-// variables I'll need:
-var secretCode, userGuess, whiteCount, blackCount, codeLength, digitRange
+var secretCode, userGuess, whiteCount, blackCount, secretCodeLength, digitRange;
+
+secretCodeLength = 4;
+digitRange = 4;
 
 // 1. click handlers and accompanying functions to record user input and reset the game
-// $('#submitGuess').click(getGuess);
-$('#submitGuess').click(render);
+$('#submitGuess').click(handleSubmit);
+$('#resetGame').click(handleReset);
 
 // 2. error handling for invalid inputs (e.g. blank fields or non numeric characters)
 
 function initializeGame() {
     generateCode();
-    console.log('sc ' + secretCode);
-    console.log('ug ' + userGuess);
-}
-
-function render() {
-    getWhiteCount(getBlackCount(secretCode, getGuess()));
+    // console.log('sc ' + secretCode);
 }
 
 // 3. code generator - done
 function generateCode() {
     secretCode = [];
-    for (var num = 0; num < 4; num++) {
-        secretCode.push(Math.floor(Math.random() * 4) + 1);
+    for (var num = 0; num < secretCodeLength; num++) {
+        secretCode.push(Math.floor(Math.random() * digitRange) + 1);
     }
-
-    // return secretCode;
+    
+    return secretCode;
 }
 
-// guess recorder
+function handleSubmit() {
+    userGuess = getGuess();
+    blackCount = getBlackCount(secretCode, userGuess);
+    whiteCount = getWhiteCount(blackCount);
+    // console.log('render ug ' + userGuess);
+    // console.log('render bc ' + blackCount);
+    // console.log('render wc ' + whiteCount);
+    render();
+}
+
+function handleReset() {
+    
+}
+
 function getGuess() {
     var guess = [];
     for (var num = 1; num < 5; num++) {
         guess.push(parseInt($('#input' + num).val()));
     }
-
-    console.log('guess ' + guess);
+    
+    // console.log('guess ' + guess);
     return guess;
 }
 
-// 4. function to iterate through guess and code to determine how many elements the two share (this will set the initial white dot count)
-function getWhiteCount(blackCount) {
-    var code = secretCode;
-    console.log('code ' + code);
-    console.log('ug in gwc ' + userGuess);
-
-    var whiteCount = 0;
-        for (var i = 0; i < secretCode.length; i++) {
-            if (userGuess[i] in code) {
-                whiteCount++;
-                code.splice(code.indexOf(userGuess));
-            }
+function getBlackCount(secretCode, userGuess) {
+    var blackCount = 0;
+    for (var i = 0; i < secretCode.length; i++) {
+        if (secretCode[i] == userGuess[i]) {
+            blackCount++;
         }
+        // secretCode[i] == userGuess[i] ? blackCount++ : blackCount = blackCount;
+    }
+    
+    // console.log('bc ' + blackCount);
+    return blackCount;
+}
 
-    console.log('wc' + whiteCount);
+function getWhiteCount(blackCount) {
+    var code = secretCode.slice();
+    // console.log('code ' + code);
+    // console.log('ug in gwc ' + userGuess);
+    
+    var whiteCount = 0;
+    for (var i = 0; i < secretCode.length; i++) {
+        if (userGuess[i] in code) {
+            whiteCount++;
+            code.splice(code.indexOf(userGuess));
+        }
+    }
+    
+    // console.log('wc initial ' + whiteCount);
     return whiteCount - blackCount;
+}
+
+function render() {
+    
 }
 
 
 // 5. function to iterate through guess and code to determine how many elements the two share in the correct position (this will generate the black dot count)
-function getBlackCount(secretCode, userGuess) {
-    var blackCount = 0;
-        for (var i = 0; i < secretCode.length; i++) {
-            secretCode[i] == userGuess[i] ? blackCount++ : blackCount = blackCount;
-        }
-    
-    console.log('bc ' + blackCount);
-    return blackCount;
-}
 
 // 6. function to subtract black dot count from white dot count (this will set the final white dot count)
 
