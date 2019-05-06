f6a462d125cd8f919220f6517edc588298d33419
Mike Feldberg <mikefeldberg@gmail.com>
2019-05-05 23:53:48 -0700

    add keyup events

--- 2019.05.02 P1 - Mastermind/js/mastermind.js
+++ 2019.05.02 P1 - Mastermind/js/mastermind.js
@@ -5,9 +5,9 @@ var secretCodeLength, digitRange, secretCode, userGuess, whiteCount, blackCount,
 secretCodeLength = 4;
 digitRange = 4;
 
-// 1. click handlers and accompanying functions to record user input and reset the game
+
 $('#submitGuess').click(handleSubmit);
-$('#resetGame').click(handleReset);
+$('#resetGame').click(initializeGame);
 
 // 2. error handling for invalid inputs (e.g. blank fields or non numeric characters)
 
@@ -16,11 +16,12 @@ function initializeGame() {
     console.log('sc ' + secretCode);
     generateInputFields();
     appendSecretCode();
+    clearFeedback();
+    initializeKeyListeners();
+    $('#input1').focus();
     turn = 0;
-
 }
 
-// 3. code generator - done
 function generateCode() {
     secretCode = [];
     for (var num = 0; num < secretCodeLength; num++) {
@@ -30,10 +31,11 @@ function generateCode() {
     return secretCode;
 }
 
-function generateInputFields() {
-    $('.ipc').remove();
+function generateInputFields() {    
+    $('#inputContainer').html('');
+
     for (var i = 1; i <= secretCodeLength; i++) {
-        $('#inputContainer').append(`<div class="ipc"><input id="input${i}" minlength="1" maxlength="1" class="userInput"></div>`);
+        $('#inputContainer').append(`<input id="input${i}" type="text" minlength="1" maxlength="1" class="userInput">`);
     }
 }
 
@@ -43,10 +45,6 @@ function handleSubmit() {
     blackCount = getBlackCount();
     whiteCount = getWhiteCount();
     render();
-}
-
-function handleReset() {
-    initializeGame();
 }
 
 function render() {
@@ -59,8 +57,7 @@ function getGuess() {
     for (var num = 1; num < 5; num++) {
         guess.push(parseInt($('#input' + num).val()));
     }
-    
-    // console.log('guess ' + guess);
+
     return guess;
 }
 
@@ -70,34 +67,22 @@ function getBlackCount() {
         if (secretCode[i] == userGuess[i]) {
             blackCount++;
         }
-        // secretCode[i] == userGuess[i] ? blackCount++ : blackCount = blackCount;
     }
-    
-    // console.log('bc ' + blackCount);
+
     return blackCount;
 }
 
 function getWhiteCount() {
     var code = secretCode.slice();
-    // console.log('gwc sc ' + secretCode);
-    // console.log('gwc code ' + code);
-    // console.log('code ' + code);
-    // console.log('ug in gwc ' + userGuess);
     
     whiteCount = 0;
     for (var i = 0; i < secretCodeLength; i++) {
         if (code.includes(userGuess[i])) {
-            // console.log(code + ' includes ' + userGuess[i]);
             whiteCount++;
-            // console.log('gwc for if wc ' + whiteCount);
             code.splice(code.indexOf(userGuess[i]), 1);
-            // console.log('gwc for if spliced code is ' + code);
-
         }
-        // console.log('gwc for code ' + code);
     }
-    
-    // console.log('wc initial ' + whiteCount);
+
     return whiteCount - blackCount;
 }
 
@@ -121,10 +106,8 @@ function appendFeedbackToRow() {
 }
 
 function appendSecretCode() {
-    // $('#secretCodeContainer').innerHTML = '';
-    // above line does not work for some reason
+    $('#secretCodeContainer').html('');
 
-    $('.mysteryPeg').remove();
     for (var i = 0; i < secretCodeLength; i++) {
         $('#secretCodeContainer').append('<div class="mysteryPeg">?</div>');
     }
@@ -147,4 +130,31 @@ function appendWhiteDots() {
     }
 }
 
+function clearFeedback() {
+    $('#feedbackContainer').html('');
+}
+
 initializeGame();
+
+function initializeKeyListeners() {
+    $('.userInput').keyup(function() {
+        if ($(this).val()) {
+            $(this).next().focus();
+        }
+    });
+    
+    $('.userInput').keyup(function(event) {
+        if (event.keyCode == 8) {
+            $(this).prev().focus();
+        }
+    });
+
+    // ($('.userInput').onblur = function() {
+    //     if (!input.value.includes('@')) { // not email
+    //       input.classList.add('invalid');
+    //       error.innerHTML = 'Please enter a correct email.'
+    //     }
+    //   };
+
+}
+
