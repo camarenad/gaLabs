cb8bab26196ae11933a9377f687bef3157434bcd
Mike Feldberg <mikefeldberg@gmail.com>
2019-05-13 22:29:24 -0700

    working recipes DB w super bonus but no reg bonus

---
+++ 2019.05.13 Express Lab/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const recipesDb = require('./data/recipesDb');
+
+app.set('view engine', 'ejs');
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', function(req, res) {
+    res.redirect('/home');
+});
+
+app.get('/home', function(req, res) {
+    res.render('home');
+});
+
+app.get('/recipes', function(req, res) {
+    res.render('recipes/index', {
+        recipes: recipesDb.getAll()
+    });
+});
+
+app.get('/recipes/:id', function(req, res) {
+    res.render('recipe', {dish: recipesDb.getOne(req.params.id)});
+});
+
+app.listen(3000, function() {
+    console.log('Listening on port 3000.');
+});
