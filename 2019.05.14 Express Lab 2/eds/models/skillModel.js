09f2eb165a1d82501429f3cd1b07ea9876b32ea9
Mike Feldberg <mikefeldberg@gmail.com>
2019-05-14 23:18:21 -0700

    nav working; no edit add or delete

---
+++ 2019.05.14 Express Lab 2/eds/models/skillModel.js
@@ -0,0 +1,44 @@
+var skills = [
+    {id: 0, skill: 'HTML', done: true},
+    {id: 1, skill: 'CSS', done: true},
+    {id: 2, skill: 'JavaScript', done: true},
+    {id: 3, skill: 'jQuery', done: true},
+    {id: 4, skill: 'Node JS', done: false},
+    {id: 5, skill: 'Express.js', done: false},
+    {id: 6, skill: 'MongoDB', done: false},
+    {id: 7, skill: 'Python', done: false},
+    {id: 8, skill: 'Django', done: false},
+    {id: 9, skill: 'SQL', done: false},
+    {id: 10, skill: 'Auth', done: false},
+    {id: 11, skill: 'Heroku', done: false},
+    {id: 12, skill: 'AJAX', done: false},
+    {id: 13, skill: 'ReactJS', done: false},
+];
+
+module.exports = {
+    getAll,
+    getOne,
+    create,
+    deleteOne,
+    update
+};
+  
+function update(id, skill) {
+    skills[id] = skill;
+}
+
+function deleteOne(id) {
+    skills.splice(id, 1);
+}
+
+function create(skill) {
+    skills.push(skill);
+}
+
+function getOne(id) {
+    return skills[id];
+}
+
+function getAll() {
+    return skills;
+}
--- 2019.05.14 Express Lab 2/eds/server.js
+++ 2019.05.14 Express Lab 2/eds/server.js
@@ -1,38 +1,33 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const recipesDb = require('./data/recipesDb');
+var express = require('express');
+// var skillController = require('./controllers/skillController')
+var bodyParser = require('body-parser');
+var path = require('path');
+var app = express();
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var methodOverride = require('method-override');
+
+// var indexRouter = require('./routes/index');
+var skillRouter = require('./routes/skillRoute');
+app.use(methodOverride(''));
 
 app.set('view engine', 'ejs');
 
-app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+// app.use(express.static(path.join(__dirname, 'views')));
 
 app.use('/assets', express.static('assets'));
 
-app.get('/', function(req, res) {
-    res.redirect('/home');
+app.post('/new', urlencodedParser, function(req, res) {
+    res.render('skills/new-success', {data: req.body});
 });
 
-app.get('/home', function(req, res) {
-    res.render('home');
-});
+// app.use('/', indexRouter);
+app.use('/', skillRouter);
 
-app.get('/about', function(req, res) {
-    res.render('about');
-});
-
-app.get('/recipes', function(req, res) {
-    res.render('recipes/index', {
-        recipes: recipesDb.getAll()
-    });
-});
+// app.use(methodOverride('_method'));
 
 
-
-app.get('/recipes/:id', function(req, res) {
-    res.render('recipes/show', {dish: recipesDb.getOne(req.params.id)});
-});
+// skillController(app);
 
 app.listen(3001, function() {
     console.log('Listening on port 3001.');
